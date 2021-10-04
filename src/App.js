f53de7350a73d493d333a2523705837c8e198957
Bright Kanyange <brightkanyange@outlook.com>
2021-10-04 15:29:59 +0300

    Added basename

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import LoginPage from "./pages/LoginPage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+const getBasename = () => {
+  return `/${process.env.PUBLIC_URL.split('/').pop()}`;
+};
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={getBasename()}>
       <Switch>
         <Route path="/" component={LoginPage} />
         <Route exact path="/guest/s/:slug" component={LoginPage} />
